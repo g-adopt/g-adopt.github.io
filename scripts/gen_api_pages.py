566e2ccc0291d67e5bd300adbaa053573d8343c8
Angus Gibson <angus@agibson.me>
2024-06-24 15:06:38 +1000

    Generate a documentation page for each API module
    This is a bit clearer to read than dumping everything on a single page.

---
+++ scripts/gen_api_pages.py
@@ -0,0 +1,15 @@
+import mkdocs_gen_files
+import pkgutil
+from pathlib import Path
+
+nav = mkdocs_gen_files.Nav()
+
+root = Path(__file__).parent.parent
+api_path = root / "g-adopt/gadopt"
+
+for m in pkgutil.iter_modules([api_path]):
+    doc_path = Path(m.name).with_suffix(".md")
+    full_doc_path = Path("documentation", doc_path)
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        print(f"::: gadopt.{m.name}", file=fd)
